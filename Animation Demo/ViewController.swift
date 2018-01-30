b1817bc9195f74bcf12fea71ca4960ec69716049
Ryan King <ryan.king@gousto.co.uk>
2018-01-30 13:37:19 +0000

    First commit

--- Animation Demo/ViewController.swift
+++ Animation Demo/ViewController.swift
@@ -1,25 +1,50 @@
-//
-//  ViewController.swift
-//  Animation Demo
-//
-//  Created by Ryan on 08/12/2017.
-//  Copyright © 2017 Ryan. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+// MARK: RaceViewController
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class RaceViewController: UIViewController {
+
+    // MARK: Outlets
+
+    @IBOutlet weak var raceButton: UIButton!
+    @IBOutlet weak var raceView: UIView!
+    @IBOutlet weak var purpleCar: UIImageView!
+    @IBOutlet weak var greyCar: UIImageView!
+    @IBOutlet weak var yellowCar: UIImageView!
+    @IBOutlet weak var purpleCarTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var greyCarTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var yellowCarTopConstraint: NSLayoutConstraint!
+
+
+    // MARK: Constants
+
+    let duration = 4.0   // Duration of the animation
+    let delay = 0.0      // Delay of the animation
+    let carHeight = 100  // Height of the race cars
+
+    // MARK: Action Methods
+
+    @IBAction func startRace(button: UIButton) {
+        let distance = raceView.frame.height - CGFloat(carHeight + 20)
+        self.view.layoutIfNeeded()
+        
+        // Purple car
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
+            self.purpleCarTopConstraint.constant = distance
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        // Grey car
+        UIView.animate(withDuration: duration, delay: delay, animations: {
+            self.greyCarTopConstraint.constant = distance
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        // Yellow car
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseOut, animations: {
+            self.yellowCarTopConstraint.constant = distance
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
